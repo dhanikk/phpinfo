086b6c44f8bae78edc718aeef461f378397c2c5b
Dhanik Keraliya <dhanikk@itpathsolutions.com>
2025-01-30 12:51:01 +0530

    Remove database information

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,8 +1,5 @@
 <?php
     use Itpathsolutions\Phpinfo\Http\Controllers\PHPServerController;
     use Illuminate\Support\Facades\Route;
-    Route::get('/dashboard/php-info', [PHPServerController::class, 'index']);
-    Route::get('/dashboard/database-info', [PHPServerController::class, 'dbinfo']);
-    Route::get('/dashboard/process-list', [PHPServerController::class, 'showprocesslist'])->name("processlist");
-
+    Route::get('/php-info', [PHPServerController::class, 'index']);
 ?>
